a568a1237fd0bf2db5aec9c71a507d30a59a4c52
poriya <poriya.t1998@gmail.com>
2023-08-17 08:04:31 +0330

    finish UI and create onClick method

---
+++ 001 project Calculator Efficient/app/src/main/java/com/poriyanasri/calculatorefficient/Expression.kt
@@ -0,0 +1,71 @@
+package com.poriyanasri.calculatorefficient
+
+import java.util.Stack
+
+class Expression(var infixExpression: MutableList<String>) {
+
+
+    private fun infixToPostfix(): String {
+        var result = ""
+        val stack = Stack<String>()
+        for (element in infixExpression) {
+            if (element.all { it.isDigit() } || element.any { it == '.' }) {
+                result += "$element "
+            } else if (element == "(") {
+                stack.push(element)
+            } else if (element == ")") {
+                while (stack.peek() != "(" && stack.isNotEmpty()) {
+                    result += "${stack.pop()}"
+                }
+                if (stack.isNotEmpty())
+                    stack.pop()
+            } else {
+                while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(element)) {
+                    result += "${stack.pop()} "
+                }
+            }
+        }
+        while (stack.isNotEmpty()) {
+            result += "${stack.pop()} "
+        }
+        return result
+    }
+
+    private fun precedence(operator: String): Int {
+        return when (operator) {
+            "*", "/" -> 2
+            "+", "-" -> 1
+            else -> -1
+        }
+
+    }
+
+    fun evaluateExpression(postfix: String): Number {
+        val stack = Stack<Double>()
+        var i = 0
+        while (i < postfix.length) {
+            if (postfix[i] == ' ') {
+                i++
+                continue
+            } else if (Character.isDigit(postfix[i])) {
+                var number = ""
+                while (Character.isDigit(postfix[i]) || postfix[i] == '.') {
+                    number += postfix[i]
+                    i++
+                }
+                stack.push(number.toDouble())
+            } else {
+                val x = stack.pop()
+                val y = stack.pop()
+                when (postfix[i]) {
+                    '*' -> stack.push(x * y)
+                    '/' -> stack.push(x / y)
+                    '+' -> stack.push(x + y)
+                    '-' -> stack.push(x - y)
+                }
+            }
+            i++
+        }
+        return if (stack.peek() / stack.peek() == 1.0) stack.peek().toInt() else stack.peek()
+    }
+}
